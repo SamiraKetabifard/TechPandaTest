463d5b3b930d6a10f23819551df0a6b56e1304c5
SamiraKetabifard <samiraketabi.fard@gmail.com>
2026-08-23 21:13:22 +0330

    done

--- src/test/java/Test1/Test4.java
+++ src/test/java/Test1/Test4.java
@@ -12,20 +12,103 @@ import java.time.Duration;
 import static org.testng.Assert.assertEquals;
 
 public class Test4 {
-    
+
     WebDriver driver;
 
     @BeforeTest
     public void setUp() {
         System.setProperty("webdriver.chrome.driver",
                 "C:\\Users\\parsian\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
     @Test
-    public void () throws Exception {
+    public void testCompareSonyXperiaAndIPhone() throws InterruptedException {
 
+        // 1. Go to http://live.techpanda.org
+        driver.get("http://live.techpanda.org");
+
+        // 2. Click on Mobile menu
+        driver.findElement(By.linkText("MOBILE")).click();
+
+        // 3. In mobile products list , click on 'Add To Compare' for 2 mobiles (Iphone & Sony Xperia)
+        // store the title of the 2 mobiles for comparison for verification later when popup page comes up
+
+        driver.findElement(By.cssSelector(
+                "a[href*='product_compare/add/product/2']")).click();
+
+        String mainMobile1 = driver.findElement(
+                By.xpath("//h2/a[@title='IPhone']")
+        ).getText(); // text captured - upperCase "IPHONE"
+
+        System.out.println("mainMobile1 = " + mainMobile1);
+
+        Thread.sleep(1000);
+
+        driver.findElement(By.cssSelector(
+                "a[href*='product_compare/add/product/1']")).click();
+
+        String mainMobile2 = driver.findElement(
+                By.xpath("//h2/a[@title='Sony Xperia']")
+        ).getText(); // text captured - upperCase "SONY XPERIA"
+
+        System.out.println("mainMobile2 = " + mainMobile2);
+
+        Thread.sleep(1000);
+
+        // 4. Click on 'COMPARE' button. A popup window opens
+        driver.findElement(By.xpath("//button[@title='Compare']")).click();
+
+        Thread.sleep(1000);
+
+        // switching to new window
+        for (String handle : driver.getWindowHandles()) {
+            driver.switchTo().window(handle);
+        }
+
+        // 5. Verify the pop-up window and check that the products are reflected in it
+        // Heading "COMPARE PRODUCTS" with selected products in it.
+
+        String strHead = "COMPARE PRODUCTS";
+
+        String compHead = driver.findElement(
+                By.cssSelector("h1")
+        ).getText();
+
+        System.out.println("compHead = " + compHead);
+
+        String popupMobile1 = driver.findElement(
+                By.xpath("//h2/a[@title='IPhone']")
+        ).getText(); // text captured is "IPHONE" in uppercase
+
+        String popupMobile2 = driver.findElement(
+                By.xpath("//h2/a[@title='Sony Xperia']")
+        ).getText(); // text captured "SONY XPERIA" in uppercase
+
+        System.out.println("popupMobile1 = " + popupMobile1);
+        System.out.println("popupMobile2 = " + popupMobile2);
+
+        Thread.sleep(1000);
+
+        // to check the popup heading/title
+        assertEquals(strHead, compHead);
+
+        // to check the 2 mobiles selected are the two in the popup
+        // this is to check the IPhone
+        assertEquals(mainMobile1, popupMobile1);
+
+        // to check the 2 mobiles selected are the two in the popup
+        // this is to check Sony X
+        assertEquals(mainMobile2, popupMobile2);
+
+        // 6. Close the Popup Windows
+        driver.findElement(
+                By.xpath("//button[@title='Close Window']")
+        ).click();
+
+        Thread.sleep(1000);
     }
 
     @AfterTest
--- src/test/java/Test1/Test5.java
+++ src/test/java/Test1/Test5.java
@@ -1,4 +1,119 @@
 package Test1;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import java.time.Duration;
+
 public class Test5 {
+
+    WebDriver driver;
+    String firstName = "samira";
+    String lastName = "ketabifard";
+    // unique email for every run
+    String email = "samira" + System.currentTimeMillis() + "@gmail.com";
+
+    @BeforeTest
+    public void setUp() {
+
+        System.setProperty(
+                "webdriver.chrome.driver",
+                "C:\\Users\\parsian\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+
+        driver = new ChromeDriver();
+        driver.manage().timeouts()
+                .implicitlyWait(Duration.ofSeconds(10));
+    }
+    @Test
+    public void testCreateAccountAndShareWishList() throws Exception {
+        // 1. Go to website
+        driver.get("http://live.techpanda.org/");
+        // 2. Click My Account
+        driver.findElement(By.linkText("MY ACCOUNT")).click();
+
+        Thread.sleep(2000);
+
+        // 3a. Click Create Account
+        driver.findElement(By.linkText("CREATE AN ACCOUNT")).click();
+
+        Thread.sleep(2000);
+
+        // 3b. Fill registration form
+
+        driver.findElement(By.id("firstname"))
+                .sendKeys(firstName);
+
+        driver.findElement(By.id("lastname"))
+                .sendKeys(lastName);
+
+        driver.findElement(By.id("email_address"))
+                .sendKeys(email);
+
+        driver.findElement(By.id("password"))
+                .sendKeys("samira20@");
+
+        driver.findElement(By.id("confirmation"))
+                .sendKeys("samira20@");
+
+        // 4. Register
+        driver.findElement(
+                By.xpath("//button[@title='Register']")).click();
+
+        Thread.sleep(2000);
+        // 5. Verify registration
+        String expectedWelcome =
+                "WELCOME, " + firstName + " " + lastName + "!";
+        String actualWelcome =
+                driver.findElement(
+                        By.cssSelector("p.welcome-msg")).getText();
+
+        System.out.println("Expected : " + expectedWelcome);
+        System.out.println("Actual   : " + actualWelcome);
+
+        Assert.assertEquals(actualWelcome.toUpperCase(), expectedWelcome.toUpperCase());
+
+        // 6. Go to TV menu
+        driver.findElement(
+                By.xpath(".//*[@id='nav']/ol/li[2]/a")).click();
+        Thread.sleep(2000);
+        // 7. Add LG LCD to wishlist
+        driver.findElement(
+                By.xpath("//li/a[@class='link-wishlist']")).click();
+        Thread.sleep(2000);
+        // 8. Share Wishlist
+        driver.findElement(
+                By.xpath("//button[@title='Share Wishlist']")).click();
+        Thread.sleep(2000);
+        // 9. Fill share form
+        driver.findElement(By.id("email_address")).clear();
+        driver.findElement(By.id("email_address")).sendKeys("support@guru99.com");
+        driver.findElement(By.id("message"))
+                .sendKeys(
+                        "Hey!! this LCD TV looks ok, check it out !!");
+        driver.findElement(
+                By.xpath("//button[@title='Share Wishlist']")).click();
+        Thread.sleep(2000);
+        // 10. Verify Wishlist shared
+        String expectedWishList =
+                "Your Wishlist has been shared.";
+        String actualWishList =
+                driver.findElement(
+                        By.xpath(
+                                ".//*[@id='top']/body/div[1]/div/div[2]/div/div[2]/div/div[1]/ul/li/ul/li/span"
+                        )).getText();
+        System.out.println("Expected Wishlist : " + expectedWishList);
+        System.out.println("Actual Wishlist   : " + actualWishList);
+
+        Assert.assertEquals(
+                actualWishList, expectedWishList);
+
+    }
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
+    }
 }
